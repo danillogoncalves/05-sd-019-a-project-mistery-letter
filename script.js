fe3c30efcebe1c393ed572f01a0e275cab0701ca
danillo <danillogonvalves001@gmail.com>
2021-12-19 18:18:24 -0300

    Décimo sétimo e décimo oitavo requisito feito.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const styleGroup = [`newspaper`, `magazine1`, `magazine2`];
 const sizeGroup = [`medium`, `big`, `reallybig`];
 const rotationGroup = [`rotateleft`, `rotateright`];
 const slopeGroup = [`skewleft`, `skewright`];
+const wordCounter = document.querySelector('#carta-contador');
 
 function createArray() {
     deleteChildrenOfP();
@@ -23,25 +24,33 @@ function deleteChildrenOfP () {
             p.removeChild(p.childNodes[i]);
         }
     }
-
-
 }
 
 function arrayInMysteriousLetter(array) {
     const p = document.querySelector('#carta-gerada');
     for (let i = 0; i < array.length; i += 1) {
         const span = document.createElement('span');
+        span.addEventListener('click', newClassStyles)
         span.classList.add(styleGroup[parseInt(Math.random()*styleGroup.length)])
         span.classList.add(sizeGroup[parseInt(Math.random()*sizeGroup.length)])
-        if (parseInt(Math.random()*2)) {
+        // if (parseInt(Math.random()*2)) {
             span.classList.add(rotationGroup[parseInt(Math.random()*rotationGroup.length)])
-        }
-        if (parseInt(Math.random()*2)) {
+        // }
+        // if (parseInt(Math.random()*2)) {
             span.classList.add(slopeGroup[parseInt(Math.random()*slopeGroup.length)])
-        }
+        // }
         span.innerHTML = array[i];
         p.appendChild(span);
     }
+    wordCounter.innerHTML = array.length;
+}
+
+function newClassStyles (event) {
+    event.target.removeAttribute('class');
+    event.target.classList.add(styleGroup[parseInt(Math.random()*styleGroup.length)])
+    event.target.classList.add(sizeGroup[parseInt(Math.random()*sizeGroup.length)])
+    event.target.classList.add(rotationGroup[parseInt(Math.random()*rotationGroup.length)])
+    event.target.classList.add(slopeGroup[parseInt(Math.random()*slopeGroup.length)])
 }
 
 buttonGenerateLetter.addEventListener('click', createArray);
