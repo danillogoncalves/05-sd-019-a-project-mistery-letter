90b4c57e455486b17b5984591b784d5bb608d6f7
danillo <danillogonvalves001@gmail.com>
2021-12-24 15:01:58 -0300

    Arrumando o ESLint e StyleLint.

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const buttonGenerateLetter = document.querySelector('#criar-carta');
 const inputLetterText = document.querySelector('#carta-texto');
-const styleGroup = [`newspaper`, `magazine1`, `magazine2`];
-const sizeGroup = [`medium`, `big`, `reallybig`];
-const rotationGroup = [`rotateleft`, `rotateright`];
-const slopeGroup = [`skewleft`, `skewright`];
+const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+const sizeGroup = ['medium', 'big', 'reallybig'];
+const rotationGroup = ['rotateleft', 'rotateright'];
+const slopeGroup = ['skewleft', 'skewright'];
 const wordCounter = document.querySelector('#carta-contador');
 
 function createArray() {
-    deleteChildrenOfP();
-    if (Math.abs(inputLetterText.value) === 0) {
-        document.querySelector('#carta-gerada').innerHTML = 'Por favor, digite o conteúdo da carta.';
-    } else {
-        let inputText = inputLetterText.value;
-        let arrayLetterWords = inputText.split(' ');
-        arrayInMysteriousLetter(arrayLetterWords);
-    }
+  deleteChildrenOfP();
+  if (Math.abs(inputLetterText.value) === 0) {
+    document.querySelector('#carta-gerada').innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    let inputText = inputLetterText.value;
+    let arrayLetterWords = inputText.split(' ');
+    arrayInMysteriousLetter(arrayLetterWords);
+  }
 }
 
 function deleteChildrenOfP () {
     const p = document.querySelector('#carta-gerada');
     if (p.childNodes.length) {
-        for (let i = p.childNodes.length -1; i >= 0; i -= 1) {
+        for (let i = p.childNodes.length - 1; i >= 0; i -= 1) {
             p.removeChild(p.childNodes[i]);
         }
     }
@@ -31,13 +31,13 @@ function arrayInMysteriousLetter(array) {
     for (let i = 0; i < array.length; i += 1) {
         const span = document.createElement('span');
         span.addEventListener('click', newClassStyles)
-        span.classList.add(styleGroup[parseInt(Math.random()*styleGroup.length)])
-        span.classList.add(sizeGroup[parseInt(Math.random()*sizeGroup.length)])
+        span.classList.add(styleGroup[parseInt(Math.random()*styleGroup.length)]);
+        span.classList.add(sizeGroup[parseInt(Math.random()*sizeGroup.length)]);
         // if (parseInt(Math.random()*2)) {
-            span.classList.add(rotationGroup[parseInt(Math.random()*rotationGroup.length)])
+            span.classList.add(rotationGroup[parseInt(Math.random()*rotationGroup.length)]);
         // }
         // if (parseInt(Math.random()*2)) {
-            span.classList.add(slopeGroup[parseInt(Math.random()*slopeGroup.length)])
+            span.classList.add(slopeGroup[parseInt(Math.random()*slopeGroup.length)]);
         // }
         span.innerHTML = array[i];
         p.appendChild(span);
@@ -45,12 +45,12 @@ function arrayInMysteriousLetter(array) {
     wordCounter.innerHTML = array.length;
 }
 
-function newClassStyles (event) {
+function newClassStyles(event) {
     event.target.removeAttribute('class');
-    event.target.classList.add(styleGroup[parseInt(Math.random()*styleGroup.length)])
-    event.target.classList.add(sizeGroup[parseInt(Math.random()*sizeGroup.length)])
-    event.target.classList.add(rotationGroup[parseInt(Math.random()*rotationGroup.length)])
-    event.target.classList.add(slopeGroup[parseInt(Math.random()*slopeGroup.length)])
+    event.target.classList.add(styleGroup[parseInt(Math.random()*styleGroup.length)]);
+    event.target.classList.add(sizeGroup[parseInt(Math.random()*sizeGroup.length)]);
+    event.target.classList.add(rotationGroup[parseInt(Math.random()*rotationGroup.length)]);
+    event.target.classList.add(slopeGroup[parseInt(Math.random()*slopeGroup.length)]);
 }
 
 buttonGenerateLetter.addEventListener('click', createArray);
